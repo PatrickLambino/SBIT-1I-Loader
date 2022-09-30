22bf258d99c1c722fac456b61316ded857aa3010
Nad Lambino <ronaldlambino0000@gmail.com>
2022-09-30 16:41:39 +0800

    Store to git

---
+++ script.js
@@ -0,0 +1,5 @@
+const loaderContainer = document.querySelector('.loader--conatiner');
+
+window.addEventListener('load', function() {
+    loaderContainer.classList.remove('fade-out-animation');
+});
